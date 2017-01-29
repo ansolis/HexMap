35b84c382ca26fac6d4941dee698d4e441ac145c
Michael <ansolis@gmail.com>
2017-01-28 19:15:07 -0800

    Drawing test

--- HexMap/Form1.cs
+++ HexMap/Form1.cs
@@ -15,6 +15,24 @@ namespace HexMap
         public Form1()
         {
             InitializeComponent();
+            //SetStyle(ControlStyles.ResizeRedraw, true);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            ////base.OnPaint(e);
+            //System.Drawing.Pen myPen = new System.Drawing.Pen(Color.Aqua);
+            //e.Graphics.DrawRectangle(myPen, new Rectangle(this.Location.X + 5,
+            //                                              this.Location.Y + 5,
+            //                                              this.Location.X + 10,
+            //                                              this.Location.Y + 10));
+
+            var p = sender as Panel;
+            var g = e.Graphics;
+
+            Brush brush = new SolidBrush(Color.DarkGreen);
+            g.FillRectangle(new SolidBrush(Color.Black), p.DisplayRectangle);
+            g.FillRectangle(brush, new Rectangle(5, 5, 15, 15));
         }
     }
 }

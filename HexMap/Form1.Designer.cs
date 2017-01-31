8014df30c333ce2fc147a4f04255d5d85a0b0eaf
Michael <ansolis@gmail.com>
2017-01-31 00:19:27 -0800

    Load hex file. Draw squares (need to fix offsets of yellow blocks)

--- HexMap/Form1.Designer.cs
+++ HexMap/Form1.Designer.cs
@@ -29,15 +29,18 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.Color.Black;
             this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -47,6 +50,17 @@
             this.panel1.Size = new System.Drawing.Size(334, 404);
             this.panel1.TabIndex = 1;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(332, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
             // 
             // button1
             // 
@@ -58,21 +72,43 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // pictureBox1
+            // textBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(332, 50);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.textBox1.Location = new System.Drawing.Point(596, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(597, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 31);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(597, 209);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(126, 31);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(596, 246);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(127, 31);
+            this.textBox4.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(777, 585);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
@@ -80,6 +116,7 @@
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,6 +124,10 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- HexMap/Form1.cs
+++ HexMap/Form1.cs
@@ -14,12 +14,22 @@ namespace HexMap
     {
         private Bitmap buffer;
         private int lastColorUsed = 0;
+        //private bool oldScrollbarVisibleValue = false;
+        private SortedDictionary<uint, Byte> map = new SortedDictionary<uint, Byte>();
+        private const int SQUARE_SIZE = 8;
+        private const int SQUARE_MARGIN = 2;
 
         public Form1()
         {
             InitializeComponent();
 
             pictureBox1.Height = 10;
+
+            panel1.AutoScroll = false;
+            panel1.HorizontalScroll.Visible = false;
+            panel1.HorizontalScroll.Enabled = false;
+            panel1.HorizontalScroll.Maximum = 0;
+            panel1.AutoScroll = true;
 
             buffer = new Bitmap(pictureBox1.Width, pictureBox1.Height);
             Graphics g = System.Drawing.Graphics.FromImage(buffer);
@@ -35,29 +45,124 @@ namespace HexMap
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int oldBufferSize = buffer.Height;
-            
-            if (buffer == null)
+            LoadLine();
+
+            int squareSize = 10;
+            int width = pictureBox1.Width;
+            int height = ((int)map.Keys.Last() / (width / squareSize)) * squareSize;
+            buffer = new Bitmap(pictureBox1.Width, height);
+            pictureBox1.Height = height;
+            Graphics g = System.Drawing.Graphics.FromImage(buffer);
+            Brush yellowBrush = new SolidBrush(Color.Yellow);
+            Brush blackBrush = new SolidBrush(Color.Black);
+
+            foreach (uint key in map.Keys)
             {
-                buffer = new Bitmap(pictureBox1.Width, 30);
+                // TODO:  Fix position calculation
+                g.FillRectangle(blackBrush, new Rectangle((int)(key * squareSize) % width,
+                                                          (int)(key * squareSize) / width,
+                                                          squareSize, 
+                                                          squareSize));
+                g.FillRectangle(yellowBrush, new Rectangle((int)(key * squareSize) % width,
+                                                           (int)(key * squareSize) / width,
+                                                           squareSize - SQUARE_MARGIN,
+                                                           squareSize - SQUARE_MARGIN));
+            }
+
+            pictureBox1.Image = buffer;
+
+
+            //int oldBufferSize = buffer.Height;
+
+            //if (buffer == null)
+            //{
+            //    buffer = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //}
+            //else
+            //{
+            //    Bitmap newBuffer = new Bitmap(pictureBox1.Width, buffer.Height + 10);
+
+            //    using (Graphics bufferGrph = Graphics.FromImage(newBuffer))
+            //        bufferGrph.DrawImageUnscaled(buffer, Point.Empty);
+            //    buffer = newBuffer;
+            //}
+
+            //pictureBox1.Height = buffer.Height;
+
+            //if (panel1.VerticalScroll.Visible != oldScrollbarVisibleValue)
+            //{
+            //    oldScrollbarVisibleValue = panel1.VerticalScroll.Visible;
+            //    // TODO:  Update only
+            //}
+            //else
+            //{
+            //    // TODO:  Redraw all
+            //}
+
+            //Graphics g = System.Drawing.Graphics.FromImage(buffer);
+            //lastColorUsed += 20;
+            //Brush brush = new SolidBrush(Color.FromArgb(lastColorUsed));
+            //g.FillRectangle(brush, new Rectangle(0, oldBufferSize, buffer.Width, 10));
+
+            //Brush yellowBrush = new SolidBrush(Color.Yellow);
+            //g.FillRectangle(yellowBrush, new Rectangle(buffer.Width - 30, oldBufferSize, 1, 10));
+            //g.FillRectangle(yellowBrush, new Rectangle(buffer.Width - 20, oldBufferSize, 1, 10));
+            //g.FillRectangle(yellowBrush, new Rectangle(buffer.Width - 10, oldBufferSize, 1, 10));
+            //g.FillRectangle(yellowBrush, new Rectangle(buffer.Width - 1, oldBufferSize, 1, 10));
+
+            //pictureBox1.Image = buffer;
+            ////pictureBox1.Refresh();
+
+
+        }
+
+        private void LoadLine()
+        {
+            string line;
+            uint addressOffset = 0;
+
+            // Read the file and display it line by line.
+            System.IO.StreamReader file =
+               new System.IO.StreamReader("C:\\Users\\ansol\\OneDrive\\Documents\\file.hex");
+
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line[0] == ':')
+                {
+                    line = line.TrimStart(':');
+                    int count = Convert.ToInt32(line.Substring(0, 2), 16);
+                    uint address = Convert.ToUInt32(line.Substring(2, 4), 16);
+                    int recType = Convert.ToInt32(line.Substring(6, 2), 16);
+                    if (recType == 4)
+                        addressOffset = Convert.ToUInt32(line.Substring(8, 4), 16) << 16;
+                    if (recType == 0)
+                    {
+                        for (uint i = 0; i < count; i++)
+                        {
+                            map.Add(addressOffset + address + i, Convert.ToByte(line.Substring((int)(8 + i * 2), 2), 16));
+                        }
+                    }
+                }
+            }
+
+            file.Close();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = ((e.X) / SQUARE_SIZE).ToString();
+            textBox2.Text = ((e.Y) / SQUARE_SIZE).ToString();
+
+            uint index = (uint)(((buffer.Width / SQUARE_SIZE) * SQUARE_SIZE - 1) * e.Y + e.X) / SQUARE_SIZE / SQUARE_SIZE;
+            textBox3.Text = index.ToString();
+            if (map.Keys.Contains(index))
+            {
+                textBox4.Text = map[index].ToString();
             }
             else
             {
-                Bitmap newBuffer = new Bitmap(buffer.Width, buffer.Height + 10);
-                using (Graphics bufferGrph = Graphics.FromImage(newBuffer))
-                    bufferGrph.DrawImageUnscaled(buffer, Point.Empty);
-                buffer = newBuffer;
+                textBox4.Text = string.Empty;
             }
-
-            pictureBox1.Height = buffer.Height;
-
-            Graphics g = System.Drawing.Graphics.FromImage(buffer);
-            lastColorUsed += 20;
-            Brush brush = new SolidBrush(Color.FromArgb(lastColorUsed));
-            g.FillRectangle(brush, new Rectangle(0, oldBufferSize, buffer.Width, 10));
-
-            pictureBox1.Image = buffer;
-            pictureBox1.Refresh();
         }
     }
 }
